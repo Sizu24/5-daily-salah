d85019fd0b0b25e72c0e39816aefd9772983826e
Sizu24 <sizu1124@gmail.com>
2024-01-07 18:50:09 -0800

    Add update to make toggle button easer to change

--- src/components/Salah/Translate.jsx
+++ src/components/Salah/Translate.jsx
@@ -5,8 +5,8 @@ const Translate = ({ prayer, handleTranslate, translate }) => {
   return(
     <div className="translate">
       <p className={`translate__title translate__title--${prayer}`}>Translate Off/On</p>
-      <div className={translate ? "toggle-button toggle-button--on" : "toggle-button"}>
-        <div className="toggle-button__handle" onClick={handleTranslate}></div>
+      <div className={translate ? "toggle-button toggle-button--on" : "toggle-button"} onClick={handleTranslate}>
+        <div className="toggle-button__handle"></div>
       </div>
     </div>
   );  
