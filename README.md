eba8c892edf5f9a88eec2895ab1227146b23fa4f
Shah Chisty <sizu1124@gmail.com>
2024-04-11 19:25:37 -0700

    Add metatags and readme

---
+++ README.md
@@ -0,0 +1,23 @@
+# 5 Daily Salah
+
+## About
+This project is built with React, Vite, and uses sass for styling.
+
+## Installation
+Install dependencies
+
+```bash
+npm install
+```
+
+## Run on local
+
+```bash
+npm run dev
+```
+
+## Build for Production
+
+```bash
+npm run build
+```
--- src/components/Basics/BasicsIntro.jsx
+++ src/components/Basics/BasicsIntro.jsx
@@ -6,11 +6,15 @@ function BasicsIntro() {
       <h3 className="section-text__title">Introduction</h3>
       <p className="section-text__body">
         This website is intended to cover the basics of the 5 daily prayers. 
-        It is meant to be a resource for Muslims who are new to praying.
+        It is meant to be a resource for Muslims who are new to praying. This can also be used as a supplement to learning from videos online.
+      </p>
+      <p className="section-text__body">
         Take it slow, and don't worry if you don't get everything right away.
         It might also help to start with just getting down the 2 rakat prayer (Fajr), since the 4 rakat prayers are basically like repeating the 2 rakat prayer.
       </p>
-
+      <p className="section-text__body">
+        <strong>Tip:</strong> Try memorizing by breaking everything into smaller parts. Memorize the first part of a Rakat, then add more as you get comfortable.
+      </p>
     </div>
   );
 }
